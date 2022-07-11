939a2419400268436f45a6c2a86a33614f6c1b09
MariaJoseRomeroR <jrr260501@gmail.com>
2022-07-10 23:05:26 -0500

    Se ha modificado el archivo usuarioService agregando las funciones en base al archivo usuarioBL.

--- Usuario/usuarioService.php
+++ Usuario/usuarioService.php
@@ -4,5 +4,60 @@
 
     class UsuarioService 
     {
+        private $usuarioDTO;
+        private $usuarioBL;
 
+        public function __construct()
+        {
+            $this->usuarioDTO = new UsuarioDTO();
+            $this->usuarioBL = new UsuarioBL();
+        }
+
+        public function create($imagen, $nombre, $a_paterno, $a_materno, $usuario, $contrasena)
+        {
+            $this->usuarioDTO ->imagen =$imagen;
+            $this->usuarioDTO ->nombre = $nombre;
+            $this->usuarioDTO ->a_paterno = $a_paterno;
+            $this->usuarioDTO ->a_materno = $a_materno;
+            $this->usuarioDTO ->usuario = $usuario;
+            $this->usuarioDTO ->contrasena = $contrasena;
+            if(($this->usuarioBL->create($this->usuarioDTO)) > 0)
+                echo json_encode($this->usuarioDTO, JSON_PRETTY_PRINT);
+            else 
+                echo json_encode(array());
+
+        }
+
+        public function read($id) 
+        {
+            $this->usuarioDTO = $this->usuarioBL->read($id);
+            echo json_encode($this->usuarioDTO, JSON_PRETTY_PRINT);
+        }
+
+        public function update($id, $imagen, $nombre, $a_paterno, $a_materno, $usuario, $contrasena)
+        {
+            $this->usuarioDTO ->id = $id;
+            $this->usuarioDTO ->imagen =$imagen;
+            $this->usuarioDTO ->nombre = $nombre;
+            $this->usuarioDTO ->a_paterno = $a_paterno;
+            $this->usuarioDTO ->a_materno = $a_materno;
+            $this->usuarioDTO ->usuario = $usuario;
+            $this->usuarioDTO ->contrasena = $contrasena;
+            if($this->usuarioBL->update($this->usuarioDTO) > 0)
+                echo json_encode($this->usuarioDTO, JSON_PRETTY_PRINT);
+            else 
+                echo json_encode(array());
+        }
+
+        public function delete($id)
+        {
+            $this->usuarioDTO ->id = $id;
+            if($this -> usuarioBL -> delete($this -> usuarioDTO -> id) > 0)
+                echo json_encode($id, JSON_PRETTY_PRINT);
+            else
+                echo json_encode(array());
+        }                                                                  
     }
+
+
+?>
