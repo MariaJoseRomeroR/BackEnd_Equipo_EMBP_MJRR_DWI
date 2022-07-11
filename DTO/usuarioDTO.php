8968acddd9e7eb114d280a3e49dd3b353d148836
MariaJoseRomeroR <jrr260501@gmail.com>
2022-07-10 22:20:45 -0500

    Se ha generado la carpeta DTO con el archivo usuarioDTO que contiene el modelo de las variables a utilizar.

---
+++ DTO/usuarioDTO.php
@@ -0,0 +1,11 @@
+<?php
+    class UsuarioDTO {
+        public $id;
+        public $imagen;
+        public $nombre;
+        public $a_paterno;
+        public $a_materno;
+        public $usuario;
+        public $contrasena;
+    }
+?>
